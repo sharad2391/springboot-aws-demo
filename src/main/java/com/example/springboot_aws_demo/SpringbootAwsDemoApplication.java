ba78b27c938dd7bba1ffcca8d9b0964018cac056
Sharad <Sharad@Bhatt-PC>
2018-06-20 08:46:21 +0530

    FirstCommit

---
+++ src/main/java/com/example/springboot_aws_demo/SpringbootAwsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot_aws_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsDemoApplication.class, args);
+	}
+}
